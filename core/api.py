3069933c1ddad252309e9f07fb38c1d56f39f28f
Vladislav <alina_nizaeva@mail.ru>
2023-06-11 13:47:04 +0500

    fix errors -> 1,2,4,5

--- core/api.py
+++ core/api.py
@@ -1,6 +1,7 @@
 import vk_api
 from random import randrange
 from vk_api.longpoll import VkLongPoll
+from vk_api.exceptions import ApiError
 
 class VKApi:
     def __init__(self, token, token_user):
@@ -15,17 +16,23 @@ class VKApi:
         """
         try:
             return self.vk2.users.get(user_id=user_id)
-        except:
-            return 'Произошла ошибка. Попробуйте снова.'
+        except ApiError as e:
+            print(f"Ошибка VK API: {e}")
+            return None
 
-    def search_users(self, age_from, age_to, city, sex, status):
+    def search_users(self, age_from, age_to, city, sex, status, count=100, offset=0):
         """
-        Ищет пользователей по заданным параметрам
+            Ищет пользователей по заданным параметрам
         """
+        # Проверка данных
+        if not isinstance(age_from, int) or not isinstance(age_to, int) or not isinstance(city, str) or not isinstance(sex, int) or not isinstance(status, int) or not isinstance(count, int) or not isinstance(offset, int):
+            raise ValueError("Неверный тип данных аргументов")
+        
         try:
-            return self.api.users.search(count=100, age_from=age_from, age_to=age_to, city=city, sex=sex, status=status)
-        except:
-            return 'Произошла ошибка. Попробуйте снова.'
+            return self.api.users.search(count=count, offset=offset, age_from=age_from, age_to=age_to, city=city, sex=sex, status=status)
+        except ApiError as e:
+            print(f"Ошибка VK API: {e}")
+            return []
 
     def get_photo_likes(self, owner_id, photo_id):
         """
@@ -33,8 +40,9 @@ class VKApi:
         """
         try:
             return self.api.likes.getList(type="photo", owner_id=owner_id, item_id=photo_id, filter="likes")
-        except:
-            return 'Произошла ошибка. Попробуйте снова.'
+        except ApiError as e:
+            print(f"Ошибка VK API: {e}")
+            return []
 
     def get_photo_comments(self, owner_id, photo_id):
         """
@@ -42,8 +50,9 @@ class VKApi:
         """
         try:
             return self.api.photos.getComments(owner_id=owner_id, photo_id=photo_id)
-        except:
-            return 'Произошла ошибка. Попробуйте снова.'
+        except ApiError as e:
+            print(f"Ошибка VK API: {e}")
+            return []
 
     def get_user_photos(self, user_id):
         """
@@ -51,11 +60,15 @@ class VKApi:
         """
         try:
             return self.api.photos.get(owner_id=user_id, album_id="profile", extended=1)
-        except:
-            return 'Произошла ошибка. Попробуйте снова.'
+        except ApiError as e:
+            print(f"Ошибка VK API: {e}")
+            return []
 
     def write_msg(self, user_id, message):
         """
         Отправляет сообщение пользователю с указанным user_id
         """
-        self.api.messages.send(user_id=user_id, message=message, random_id=randrange(10 ** 7))
+        try:
+            self.vk2.messages.send(user_id=user_id, message=message, random_id=randrange(10 ** 7))
+        except ApiError as e:
+            print(f"Ошибка VK API: {e}")
--- handlers/messages.py
+++ handlers/messages.py
@@ -8,6 +8,14 @@ class MessageHandler:
         self.users = Users(self.api, Database())
         self.photos = Photos(self.api)
 
+        self.search_params = {
+            'age_from': None,
+            'age_to': None,
+            'city': None,
+            'sex': None,
+            'status': None
+        }
+
     def handle(self, event):
         """
         Обрабатывает входящее сообщение
@@ -20,11 +28,44 @@ class MessageHandler:
             self.api.write_msg(event.user_id, "Пока((")
         elif request.startswith('find'):
             params = request.split()[1:]  # разбиваем сообщение на части
-            age_from, age_to, city, sex, status = params
-            users = self.users.search_users(age_from, age_to, city, sex, status)
-            for user in users:
+            if len(params) < 5:
+                self.api.write_msg(event.user_id, "Недостаточно параметров для поиска. Введите параметры в следующем формате:\n"
+                                                 "find [age_from] [age_to] [city] [sex] [status]")
+                return
+
+            self.search_params['age_from'], self.search_params['age_to'], self.search_params['city'], \
+                self.search_params['sex'], self.search_params['status'] = params
+
+            # Код для отправки пользователю с сохранением и поэтапным выводом анкет пользователей
+            offset = 0
+            users = self.search_users(**self.search_params, count=50, offset=offset)
+            user_list = []
+
+            while users:
+                user_list.extend(users)
+                offset += 50
+                users = self.search_users(**self.search_params, count=50, offset=offset)
+
+            for user in user_list:
                 top_photos = self.photos.get_top_photos(user['id'])
                 self.api.write_msg(event.user_id, f"Найден пользователь {user['id']}. Топ фотографии: {top_photos}")
+
+        elif request.startswith('set'):
+            params = request.split()[1:]  # разбиваем сообщение на части
+            if len(params) != 2:
+                self.api.write_msg(event.user_id, "Некорректный формат команды. Введите команду в следующем формате:\n"
+                                                 "set [параметр] [значение]")
+                return
+
+            param, value = params
+            if param not in self.search_params:
+                self.api.write_msg(event.user_id, f"Некорректный параметр: {param}. Доступные параметры: {', '.join(self.search_params.keys())}")
+                return
+
+            self.search_params[param] = value
+            self.api.write_msg(event.user_id, f"Параметр {param} установлен на значение {value}")
+        elif request == 'show':
+            self.api.write_msg(event.user_id, f"Текущие параметры поиска:\n{self.search_params}")
         else:
             self.api.write_msg(event.user_id, "Не понял вашего запроса...")
 
